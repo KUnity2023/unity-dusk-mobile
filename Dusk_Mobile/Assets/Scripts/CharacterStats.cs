528d36ccacfffa0d9b4ad1aedd12971cde9174f8
DESKTOP-BJNECTQ\Lee Seung jin <3daysjin@gmail.com>
2023-11-29 09:45:39 +0900

    보스 사망, 플레이어 사망 완료

--- Dusk_Mobile/Assets/Scripts/CharacterStats.cs
+++ Dusk_Mobile/Assets/Scripts/CharacterStats.cs
@@ -18,5 +18,14 @@ public class CharacterStats : MonoBehaviour
         curHealth -= damage;
         Debug.Log(curHealth);
         GetComponent<Animator>().SetTrigger("Hurt");
+        if(curHealth <= 0){
+           StartCoroutine(Death());
+        }
+    }
+    IEnumerator Death(){
+        GetComponent<Animator>().SetTrigger("Death");
+        yield return new WaitForSeconds(6f);
+        this.gameObject.SetActive(false);
+        //사망시 추가 이벤트 자리
     }
 }
