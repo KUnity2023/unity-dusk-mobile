f83cdcf7a5444ac13ec9b8469627e18315ec69e0
DESKTOP-BJNECTQ\Lee Seung jin <3daysjin@gmail.com>
2023-12-03 07:03:12 +0900

    Spell 피격 타이밍 개선

--- Dusk_Mobile/Assets/Spellcast.cs
+++ Dusk_Mobile/Assets/Spellcast.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AdaptivePerformance;
 
 public class Spellcast : MonoBehaviour
 {
-    //public CapsuleCollider2D CapsuleCol;
     public BoxCollider2D boxCollider2D;
     public LayerMask attackMask;
     // Start is called before the first frame update
     private void Start() {
-        //CapsuleCol = GetComponent<CapsuleCollider2D>();
         attackMask = LayerMask.GetMask("Player");
         boxCollider2D = GetComponent<BoxCollider2D>();
     }
@@ -18,22 +17,6 @@ public class Spellcast : MonoBehaviour
         Destroy(gameObject,0.1f);
     }
 
-    public void detectPlayer(){
-        //Collider2D[] colInfos = Physics2D.OverlapCapsuleAll(CapsuleCol.offset,CapsuleCol.size,CapsuleCol.direction,0f,attackMask);
-        Collider2D colInfo = Physics2D.OverlapBox(boxCollider2D.offset, boxCollider2D.size,0,attackMask);
-
-        //foreach(Collider2D colInfo in colInfos){
-        Debug.Log(colInfo);
-
-        if(colInfo.CompareTag("Player")){
-            Debug.Log("Attacked by spell2");
-            colInfo.GetComponent<CharacterStats>().TakeDamage(20);
-            colInfo.GetComponent<CharacterManager>().OnDamaged(transform.position);
-            //맞은 대상의 레이어를 잠시 바꾸고 일정시간 데미지가 안들어가도록
-        }
-        //}
-        
-    }
     public void OnTriggerEnter2D(Collider2D collision){
         Debug.Log(collision);
         if(collision.CompareTag("Player")){
